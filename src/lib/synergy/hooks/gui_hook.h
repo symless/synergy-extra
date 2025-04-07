5010fa381d744c5ebdbf2fdf678847e8ec43d66c
Nick Bolton <nick@symless.com>
2025-04-07 13:54:14 +0100

    Update copyright year in gui_hook.h to reflect 2025

--- src/lib/synergy/hooks/gui_hook.h
+++ src/lib/synergy/hooks/gui_hook.h
@@ -1,6 +1,6 @@
 /*
  * Synergy -- mouse and keyboard sharing utility
- * Copyright (C) 2024 Symless Ltd.
+ * Copyright (C) 2024 - 2025 Symless Ltd.
  *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
